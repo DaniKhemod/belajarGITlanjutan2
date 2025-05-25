a2885db8e3957fcfe6cce8d0f3ad3b18c40fbaa6
Dani Khemod <daniekekan@gmail.com>
2025-05-25 17:43:32 +0700

    menambah fitur perkalian

--- fungsi.js
+++ fungsi.js
@@ -1,3 +1,9 @@
-console.log('fungsi pertama');
 const add = (a, b) => a + b;
 console.log(add(1,2));
+
+const subtract = (a, b) => a -b;
+console.log(subtract(10,5));
+
+const multiply = (a, b) => a * b;
+console.log(multiply(5,7));
+
